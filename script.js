bae6f483390893ceee9bc6204d9aeb43f59615d0
Samir Mandal <113394127+Samirmandal84@users.noreply.github.com>
2022-12-10 20:06:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// alert(`❤️Good Evening Babu❤️`);
+alert(`Hi Sara🥰`);
+alert(`Acha ek Baat Bolu🥺👉🏻👈🏻`);
+alert(`Sara, I'm Really SORRY for yesterday😔`);
+alert(`🤭Meri Cutie Sara🥰`);
+alert(`MuuuAAAAAhhhhhhhhhhhhh❤️`);
+alert(`Hum Tumhare Bina Nahi Rehh sakte🥺`);
+alert(`❤️Sara, Humko chodd ke Kabhi Matt Jana🥺🫶`);
+prompt(`Will You be mine Forever Sara❤️`);
+alert(`My Second Half❤️`);
